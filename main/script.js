4751de8424e0b296f4181366c249ac980e6799a3
PYramid8955 <cs.is.coding@gmail.com>
2023-06-22 16:22:25 +0300

    Change background - Add letter-falling background animation (as in 404) - Start stylizing the options in main menu (they're not done yet though)

--- main/script.js
+++ main/script.js
@@ -24,3 +24,21 @@ function option(op) {
         }
 }
 
+async function letters() {
+    for (i of document.getElementById("letters").children) {
+        i.style.top = Math.floor(Math.random() * (window.innerHeight - 2 * i.offsetHeight) + i.offsetHeight) - window.innerHeight + "px";
+        i.style.left = Math.floor(Math.random() * (window.innerWidth - 2 * i.offsetWidth) + i.offsetWidth) + "px";
+    }
+    setInterval(() => {
+        for (i of document.getElementById("letters").children) {
+            let y = parseInt(i.style.top.replace("px", ""));
+            if (y + i.offsetHeight <= window.innerHeight - i.offsetHeight) {
+                i.style.top = y + i.offsetHeight + "px";
+            } else {
+                i.style.top = Math.floor(Math.random() * (window.innerHeight - 2 * i.offsetHeight) + i.offsetHeight) - window.innerHeight + "px";
+                i.style.left = Math.floor(Math.random() * (window.innerWidth - 2 * i.offsetWidth) + i.offsetWidth) + "px";
+            }
+        }
+
+    }, 100)
+}
